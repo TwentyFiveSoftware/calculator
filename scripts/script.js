78064ec537f246d6e8775deebb9a0ce2c3b3bbc1
TwentyFive <TwentyFiveSoftware@gmail.com>
2020-07-24 18:19:33 +0200

    Added click events for buttons.

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,10 @@ const app = new Vue({
     data: {
         input: '1234,5 + 0,5',
         output: '1235'
+    },
+    methods: {
+        clickButton(button) {
+            console.log(button)
+        }
     }
 })
